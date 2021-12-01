1fd853af41fda5f8b25c91969ee228896c0b6422
Marian Brändle <marianbraendle@gmx.de>
2021-12-01 09:46:33 +0100

    FIX: comment

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -10,7 +10,7 @@ fn main() {
     let sums: Vec<i32> = data.windows(3).map(|x| x.iter().sum()).collect();
     let b = sums.windows(2).filter(|x| x[0] < x[1]).count();
     dbg!(b);
-    // or use the fact that (x1 + x2 + x3) - (x0 + x1 + x2) = x3 - x1
+    // or use the fact that (x1 + x2 + x3) - (x0 + x1 + x2) = x3 - x0
     let b = data.windows(4).filter(|x| x[0] < x[3]).count();
     dbg!(b);
 }
