cb25112df854b5a2544f21a2885d5c2b56e27b6d
Marian Brändle <marianbraendle@gmx.de>
2021-12-01 09:34:12 +0100

    CHG: removed unnecessary map

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -4,10 +4,10 @@ fn main() {
     let raw = fs::read_to_string("assets/day01.txt").expect("could not open file!");
     let data: Vec<i32> = raw.lines().map(|x| x.parse().unwrap()).collect();
 
-    let a = data.windows(2).map(|x| x[1] - x[0]).filter(|&x| x > 0).count();
+    let a = data.windows(2).filter(|x| x[0] < x[1]).count();
     dbg!(a);
 
     let sums: Vec<i32> = data.windows(3).map(|x| x.iter().sum()).collect();
-    let b = sums.windows(2).map(|x| x[1] - x[0]).filter(|&x| x > 0).count();
+    let b = sums.windows(2).filter(|x| x[0] < x[1]).count();
     dbg!(b);
 }
