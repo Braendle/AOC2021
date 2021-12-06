f6e0a22a6eff4eb65304502d8e875ca079d4cb0d
Marian Brändle <marianbraendle@gmx.de>
2021-12-06 07:39:26 +0100

    FIX: typo

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -4,12 +4,12 @@ use std::{collections::VecDeque, fs};
 fn main() {
     let raw = fs::read_to_string("assets/day06.txt").expect("could not open file!");
     let ages: Vec<usize> = raw.split(',').map(|x| x.parse().unwrap()).collect();
-    dbg!(laterfish_count(&ages, 80));
-    dbg!(laterfish_count(&ages, 256));
-    dbg!(laterfish_count(&ages, 1_000_000_000)); // just to test time complexity
+    dbg!(laternfish_count(&ages, 80));
+    dbg!(laternfish_count(&ages, 256));
+    dbg!(laternfish_count(&ages, 1_000_000_000)); // just to test time complexity
 }
 
-fn laterfish_count(ages: &[usize], days: usize) -> usize {
+fn laternfish_count(ages: &[usize], days: usize) -> usize {
     let mut counts: VecDeque<usize> = repeat_n(0, 9).collect();
     for age in ages {
         counts[*age] += 1;
