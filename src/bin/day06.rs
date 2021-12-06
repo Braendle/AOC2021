b939329b4f996f8cb0e31465f2a525c7972b57cc
Marian Brändle <marianbraendle@gmx.de>
2021-12-06 07:37:04 +0100

    CHG: reduce time complexity by using VecDeque

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -1,15 +1,16 @@
-use std::fs;
+use itertools::repeat_n;
+use std::{collections::VecDeque, fs};
 
 fn main() {
     let raw = fs::read_to_string("assets/day06.txt").expect("could not open file!");
-    // let raw = "3,4,3,1,2";
     let ages: Vec<usize> = raw.split(',').map(|x| x.parse().unwrap()).collect();
     dbg!(laterfish_count(&ages, 80));
     dbg!(laterfish_count(&ages, 256));
+    dbg!(laterfish_count(&ages, 1_000_000_000)); // just to test time complexity
 }
 
 fn laterfish_count(ages: &[usize], days: usize) -> usize {
-    let mut counts = [0; 9];
+    let mut counts: VecDeque<usize> = repeat_n(0, 9).collect();
     for age in ages {
         counts[*age] += 1;
     }
